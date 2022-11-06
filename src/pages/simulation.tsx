7f2d026af25f4102a9fb84414f4f50cef3a02d34
GGboi <ggboi410@gmail.com>
2022-11-06 15:26:05 -0800

    Rough GUI for simulation

--- src/pages/simulation.tsx
+++ src/pages/simulation.tsx
@@ -1,20 +1,58 @@
 import type { NextPage } from "next";
+import { useState } from "react";
 
 import Header from "src-components/Header";
 import Simulate from "@/../scripts/vote-simulation";
 
 const Simulation: NextPage = () => {
-  //Give Options for what vote counts we want to check: 1-D Array
-  const N = [500, 1000, 2000];
-  //Give option for how large of dao members we want? Current is 63, up to 100 max
-  const P = 63;
-
-  const options = {
+  const [voteNumbers, setVoteNumber] = useState<number[]>();
+  const [testSize, setTestSize] = useState<number>();
+  const [options, setOptions] = useState({
     pureRandom: false,
     maxAppearances: false,
     closeInRank: false,
+  });
+  const [hasSubmitted, setHasSubmitted] = useState(false);
+  const maxVotes = 3000;
+  const maxDaoSize = 100;
+
+  //Add Errors for if you go over the max limit
+  const inputChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    if (name === "VoteCounts") {
+      const numberArray = value
+        .split(",")
+        .map((number) => {
+          return Number(number);
+        })
+        .filter((item) => item < maxVotes);
+
+      setVoteNumber(numberArray);
+    }
+
+    if (name === "DaoSize") {
+      const size = Number(value);
+      if (size < maxDaoSize) {
+        setTestSize(size);
+      }
+    }
+
+    if (name === "pureRandom") {
+      setOptions({ ...options, pureRandom: !options.pureRandom });
+    }
+    if (name === "maxAppearances") {
+      setOptions({ ...options, maxAppearances: !options.maxAppearances });
+    }
+    if (name === "closeInRank") {
+      setOptions({ ...options, closeInRank: !options.closeInRank });
+    }
   };
-  console.log("test");
+
+  const onSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setHasSubmitted(true);
+  };
 
   //Flex Your Muscles - Give options for what algorithm changes we want to see: JSON object
   //Pure Random, max appearances, mix of +-5 rankings & random
@@ -27,6 +65,74 @@ const Simulation: NextPage = () => {
   return (
     <>
       <Header />
+      {!hasSubmitted && (
+        <div className=" flex flex-col items-center">
+          <form className="" onSubmit={(e) => onSubmitHandler(e)}>
+            <div className="flex justify-center p-2 text-xl">
+              Configure the Simulation
+            </div>
+
+            <div className="w-100 flex flex-grow flex-col">
+              <label>Number of Votes to test</label>
+              <input
+                className="m-2   p-1 text-black"
+                name="VoteCounts"
+                onChange={(e) => inputChangeHandler(e)}
+              />
+            </div>
+            <div className="w-100 flex  flex-col">
+              <label>Number of Dao Members to test</label>
+              <input
+                className="m-2   p-1 text-black"
+                name="DaoSize"
+                onChange={(e) => inputChangeHandler(e)}
+              />
+            </div>
+            <div className="w-100 m-2 flex flex-col">
+              <div className="flex justify-between">
+                <label htmlFor="pureRandom">Purely Random</label>
+
+                <input
+                  className=""
+                  type={"checkbox"}
+                  id={"pureRandom"}
+                  checked={options.pureRandom}
+                  onChange={(e) => inputChangeHandler(e)}
+                  name="pureRandom"
+                />
+              </div>
+              <div className="flex justify-between">
+                <label htmlFor="maxAppearances">Cap Maximum Appearances</label>
+
+                <input
+                  type={"checkbox"}
+                  id={"maxAppearances"}
+                  checked={options.maxAppearances}
+                  onChange={(e) => inputChangeHandler(e)}
+                  name="maxAppearances"
+                />
+              </div>
+              <div className="flex justify-between">
+                <label htmlFor="closeInRank">Vote Pairs have close rank</label>
+                <input
+                  type={"checkbox"}
+                  id={"closeInRank"}
+                  checked={options.closeInRank}
+                  onChange={(e) => inputChangeHandler(e)}
+                  name="closeInRank"
+                />
+              </div>
+            </div>
+
+            <button className="button" type="submit">
+              Submit
+            </button>
+          </form>
+        </div>
+      )}
+      {hasSubmitted && (
+        <div className=" flex flex-col items-center">Test submitted</div>
+      )}
     </>
   );
 };
